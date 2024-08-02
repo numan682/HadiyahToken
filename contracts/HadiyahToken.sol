45040edc1a3d823597086ff071a10ef2b0c73f1d
Jahidul Hasan Numan <jhnuman00@gmail.com>
2024-08-02 16:13:30 +0600

    Update HadiyahToken.sol

--- contracts/HadiyahToken.sol
+++ contracts/HadiyahToken.sol
@@ -1,11 +1,5 @@
-// SPDX-License-Identifier: MIT
-
 pragma solidity ^0.5.0;
 
-/**
- * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
- * the optional functions; to access them see `ERC20Detailed`.
- */
 interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
@@ -17,29 +11,17 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-pragma solidity ^0.5.0;
-
-/**
- * @dev Wrappers over Solidity's arithmetic operations with added overflow checks.
- * Arithmetic operations in Solidity wrap on overflow. This can easily result
- * in bugs, because programmers usually assume that an overflow raises an error,
- * which is the standard behavior in high level programming languages.
- * `SafeMath` restores this intuition by reverting the transaction when an
- * operation overflows.
- */
 library SafeMath {
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         require(c >= a, "SafeMath: addition overflow");
         return c;
     }
-
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b <= a, "SafeMath: subtraction overflow");
         uint256 c = a - b;
         return c;
     }
-
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
             return 0;
@@ -48,24 +30,17 @@ library SafeMath {
         require(c / a == b, "SafeMath: multiplication overflow");
         return c;
     }
-
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b > 0, "SafeMath: division by zero");
         uint256 c = a / b;
         return c;
     }
-
     function mod(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b != 0, "SafeMath: modulo by zero");
         return a % b;
     }
 }
 
-pragma solidity ^0.5.0;
-
-/**
- * @dev Implementation of the `IERC20` interface.
- */
 contract ERC20 is IERC20 {
     using SafeMath for uint256;
 
@@ -114,13 +89,33 @@ contract ERC20 is IERC20 {
     function _transfer(address sender, address recipient, uint256 amount) internal {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        uint256 developerFee = amount.mul(5).div(100); // 5% developer fee
+        uint256 liquidityFee = amount.mul(5).div(100); // 5% liquidity fee
+        uint256 marketingFee = amount.mul(5).div(100); // 5% marketing fee
+        uint256 burnFee = amount.mul(1).div(100); // 1% burn fee
+
+        uint256 totalFee = developerFee.add(liquidityFee).add(marketingFee).add(burnFee);
+        uint256 amountAfterFee = amount.sub(totalFee);
+
         _balances[sender] = _balances[sender].sub(amount);
-        _balances[recipient] = _balances[recipient].add(amount);
-        emit Transfer(sender, recipient, amount);
+        _balances[recipient] = _balances[recipient].add(amountAfterFee);
+        _balances[developerAddress] = _balances[developerAddress].add(developerFee);
+        _balances[liquidityAddress] = _balances[liquidityAddress].add(liquidityFee);
+        _balances[marketingAddress] = _balances[marketingAddress].add(marketingFee);
+
+        _totalSupply = _totalSupply.sub(burnFee);
+
+        emit Transfer(sender, recipient, amountAfterFee);
+        emit Transfer(sender, developerAddress, developerFee);
+        emit Transfer(sender, liquidityAddress, liquidityFee);
+        emit Transfer(sender, marketingAddress, marketingFee);
+        emit Transfer(sender, address(0), burnFee);
     }
 
     function _mint(address account, uint256 amount) internal {
         require(account != address(0), "ERC20: mint to the zero address");
+
         _totalSupply = _totalSupply.add(amount);
         _balances[account] = _balances[account].add(amount);
         emit Transfer(address(0), account, amount);
@@ -128,6 +123,7 @@ contract ERC20 is IERC20 {
 
     function _burn(address account, uint256 value) internal {
         require(account != address(0), "ERC20: burn from the zero address");
+
         _totalSupply = _totalSupply.sub(value);
         _balances[account] = _balances[account].sub(value);
         emit Transfer(account, address(0), value);
@@ -136,6 +132,7 @@ contract ERC20 is IERC20 {
     function _approve(address owner, address spender, uint256 value) internal {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
+
         _allowances[owner][spender] = value;
         emit Approval(owner, spender, value);
     }
@@ -146,18 +143,35 @@ contract ERC20 is IERC20 {
     }
 }
 
-pragma solidity ^0.5.0;
-
-/**
- * @title HadiyahToken
- */
 contract HadiyahToken is ERC20 {
-    string private _name = "HadiyahToken";
-    string private _symbol = "HDT";
-    uint8 private _decimals = 18;
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
 
-    constructor(uint256 totalSupply, address payable feeReceiver, address tokenOwnerAddress) public payable {
+    address public developerAddress;
+    address public liquidityAddress;
+    address public marketingAddress;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals,
+        uint256 totalSupply,
+        address payable feeReceiver,
+        address tokenOwnerAddress,
+        address _developerAddress,
+        address _liquidityAddress,
+        address _marketingAddress
+    ) public payable {
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
+        developerAddress = _developerAddress;
+        liquidityAddress = _liquidityAddress;
+        marketingAddress = _marketingAddress;
+
         _mint(tokenOwnerAddress, totalSupply);
+
         feeReceiver.transfer(msg.value);
     }
 
