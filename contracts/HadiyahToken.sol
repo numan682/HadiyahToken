8881fc7e3f3bd75dacbfa453bcedd154802a9335
Jahidul Hasan Numan <jhnuman00@gmail.com>
2024-08-06 02:42:25 +0600

    Update HadiyahToken.sol

--- contracts/HadiyahToken.sol
+++ contracts/HadiyahToken.sol
@@ -1,193 +1,93 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-library SafeMath {
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a, "SafeMath: addition overflow");
-        return c;
-    }
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b <= a, "SafeMath: subtraction overflow");
-        uint256 c = a - b;
-        return c;
-    }
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        require(c / a == b, "SafeMath: multiplication overflow");
-        return c;
-    }
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b > 0, "SafeMath: division by zero");
-        uint256 c = a / b;
-        return c;
-    }
-    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b != 0, "SafeMath: modulo by zero");
-        return a % b;
-    }
-}
-
-contract ERC20 is IERC20 {
-    using SafeMath for uint256;
-
-    mapping (address => uint256) private _balances;
-    mapping (address => mapping (address => uint256)) private _allowances;
-    uint256 private _totalSupply;
-
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
-    }
-
-    function transfer(address recipient, uint256 amount) public returns (bool) {
-        _transfer(msg.sender, recipient, amount);
-        return true;
-    }
-
-    function allowance(address owner, address spender) public view returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
-    function approve(address spender, uint256 value) public returns (bool) {
-        _approve(msg.sender, spender, value);
-        return true;
-    }
-
-    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
-        _transfer(sender, recipient, amount);
-        _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount));
-        return true;
-    }
-
-    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
-        _approve(msg.sender, spender, _allowances[msg.sender][spender].add(addedValue));
-        return true;
-    }
-
-    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
-        _approve(msg.sender, spender, _allowances[msg.sender][spender].sub(subtractedValue));
-        return true;
-    }
-
-    function _transfer(address sender, address recipient, uint256 amount) internal {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        uint256 developerFee = amount.mul(5).div(100); // 5% developer fee
-        uint256 liquidityFee = amount.mul(5).div(100); // 5% liquidity fee
-        uint256 marketingFee = amount.mul(5).div(100); // 5% marketing fee
-        uint256 burnFee = amount.mul(1).div(100); // 1% burn fee
-
-        uint256 totalFee = developerFee.add(liquidityFee).add(marketingFee).add(burnFee);
-        uint256 amountAfterFee = amount.sub(totalFee);
-
-        _balances[sender] = _balances[sender].sub(amount);
-        _balances[recipient] = _balances[recipient].add(amountAfterFee);
-        _balances[developerAddress] = _balances[developerAddress].add(developerFee);
-        _balances[liquidityAddress] = _balances[liquidityAddress].add(liquidityFee);
-        _balances[marketingAddress] = _balances[marketingAddress].add(marketingFee);
-
-        _totalSupply = _totalSupply.sub(burnFee);
-
-        emit Transfer(sender, recipient, amountAfterFee);
-        emit Transfer(sender, developerAddress, developerFee);
-        emit Transfer(sender, liquidityAddress, liquidityFee);
-        emit Transfer(sender, marketingAddress, marketingFee);
-        emit Transfer(sender, address(0), burnFee);
-    }
-
-    function _mint(address account, uint256 amount) internal {
-        require(account != address(0), "ERC20: mint to the zero address");
-
-        _totalSupply = _totalSupply.add(amount);
-        _balances[account] = _balances[account].add(amount);
-        emit Transfer(address(0), account, amount);
-    }
-
-    function _burn(address account, uint256 value) internal {
-        require(account != address(0), "ERC20: burn from the zero address");
-
-        _totalSupply = _totalSupply.sub(value);
-        _balances[account] = _balances[account].sub(value);
-        emit Transfer(account, address(0), value);
-    }
-
-    function _approve(address owner, address spender, uint256 value) internal {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
-
-        _allowances[owner][spender] = value;
-        emit Approval(owner, spender, value);
-    }
-
-    function _burnFrom(address account, uint256 amount) internal {
-        _burn(account, amount);
-        _approve(account, msg.sender, _allowances[account][msg.sender].sub(amount));
-    }
-}
-
-contract HadiyahToken is ERC20 {
-    string private _name;
-    string private _symbol;
-    uint8 private _decimals;
-
-    address public developerAddress;
-    address public liquidityAddress;
-    address public marketingAddress;
+contract HadiyahToken is ERC20, Ownable {
+    uint256 public tokenPriceInINR; // Price of one token in INR (Indian Rupees)
+    address internal immutable developerAddress;
+    address internal immutable liquidityAddress;
+    address internal immutable marketingAddress;
 
     constructor(
-        string memory name,
-        string memory symbol,
-        uint8 decimals,
-        uint256 totalSupply,
-        address payable feeReceiver,
-        address tokenOwnerAddress,
-        address _developerAddress,
-        address _liquidityAddress,
-        address _marketingAddress
-    ) public payable {
-        _name = name;
-        _symbol = symbol;
-        _decimals = decimals;
+    string memory tokenName,
+    string memory tokenSymbol,
+    uint256 totalSupply,
+    address tokenOwnerAddress,
+    address _developerAddress,
+    address _liquidityAddress,
+    address _marketingAddress,
+    uint256 _tokenPriceInINR
+) ERC20(tokenName, tokenSymbol) Ownable(tokenOwnerAddress)  {
         developerAddress = _developerAddress;
         liquidityAddress = _liquidityAddress;
         marketingAddress = _marketingAddress;
+        tokenPriceInINR = _tokenPriceInINR;
 
+        // Mint the initial supply to the token owner
         _mint(tokenOwnerAddress, totalSupply);
+    }
 
-        feeReceiver.transfer(msg.value);
+    function initializeLiquidity(uint256 liquidityAmount) public onlyOwner {
+        // Add tokens to the liquidity pool
+        transfer(liquidityAddress, liquidityAmount * 10 ** decimals());
+    }
+
+    function setTokenPriceInINR(uint256 newPrice) public onlyOwner {
+        tokenPriceInINR = newPrice;
+    }
+
+    function buyTokens() public payable {
+        require(msg.value > 0, "Send ETH to buy tokens");
+
+        uint256 tokensToBuy = (msg.value * tokenPriceInINR) / 10**18; // Adjust for decimals
+        require(tokensToBuy <= balanceOf(address(this)), "Not enough tokens in the reserve");
+
+        transfer(msg.sender, tokensToBuy);
     }
 
     function burn(uint256 value) public {
         _burn(msg.sender, value);
     }
 
-    function name() public view returns (string memory) {
-        return _name;
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal   {
+        require(from!= address(0), "ERC20: transfer from the zero address");
+        require(to!= address(0), "ERC20: transfer to the zero address");
+
+        uint256 developerFee = (amount * 5) / 100; // 5% developer fee
+        uint256 liquidityFee = (amount * 5) / 100; // 5% liquidity fee
+        uint256 marketingFee = (amount * 5) / 100; // 5% marketing fee
+        uint256 burnFee = (amount * 1) / 100; // 1% burn fee
+
+
+        if (developerFee > 0) {
+            _transfer(from, developerAddress, developerFee);
+        }
+        if (liquidityFee > 0) {
+            _transfer(from, liquidityAddress, liquidityFee);
+        }
+        if (marketingFee > 0) {
+            _transfer(from, marketingAddress, marketingFee);
+        }
+        if (burnFee > 0) {
+            _burn(from, burnFee);
+        }
     }
 
-    function symbol() public view returns (string memory) {
-        return _symbol;
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        _approve(msg.sender, spender, allowance(msg.sender, spender) + addedValue);
+        return true;
     }
 
-    function decimals() public view returns (uint8) {
-        return _decimals;
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        uint256 currentAllowance = allowance(msg.sender, spender);
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+        _approve(msg.sender, spender, currentAllowance - subtractedValue);
+        return true;
     }
 }
