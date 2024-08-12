53ce56b6534e3e604ea3a44f0811aef9eeec3f01
Jahidul Hasan Numan <jhnuman00@gmail.com>
2024-08-12 23:15:28 +0600

    Update HadiyahTokenPreSale.sol

--- contracts/HadiyahTokenPreSale.sol
+++ contracts/HadiyahTokenPreSale.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.20;
 
 import "./Address.sol"; // Ensure you import the Address library
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract HadiyahTokenPreSale {
     address private immutable targetAddress;
